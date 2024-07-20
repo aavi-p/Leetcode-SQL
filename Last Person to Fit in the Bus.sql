544b884821919e95d2f04cd851550db5960ad5d4
aavi-p <paavishkar@gmail.com>
2024-07-20 01:13:39 -0400

    Last Person to Fit in the Bus.sql

--- Last Person to Fit in the Bus.sql
+++ Last Person to Fit in the Bus.sql
@@ -14,12 +14,9 @@
 -- turn determines the order of which the people will board the bus, where turn=1 denotes the first person to board and turn=n denotes the last person to board.
 -- weight is the weight of the person in kilograms.
  
-
 -- There is a queue of people waiting to board a bus. However, the bus has a weight limit of 1000 kilograms, so there may be some people who cannot board.
-
--- Write a solution to find the person_name of the last person that can fit on the bus without exceeding the weight limit. The test cases are generated such that the first person does not exceed the weight limit.
-
--- The result format is in the following example.
+-
+- Write a solution to find the person_name of the last person that can fit on the bus without exceeding the weight limit. The test cases are generated such that the first person does not exceed the weight limit.
 
 SELECT person_name FROM (
 SELECT person_name, SUM(Weight) OVER (ORDER BY turn) AS total_weight
